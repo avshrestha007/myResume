ac49a8df996e8da3521080cf087a7db03b60c886
avshrestha007 <avshrestha2@gmail.com>
2023-12-22 13:42:03 +0530

    add background mode button

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("Hello This is my Resume")
+alert("Hello This is my Resume");
+
+let mode = document.querySelector("body");
+let currMode ="Light";
+
+mode.addEventListener("click", () => {
+        if(currMode === "Light"){
+        currMode = "Dark";
+        document.querySelector("body").style.backgroundColor = "black";
+    } else{
+        currMode = "Light";
+        document.querySelector("body").style.backgroundColor = "white";
+    }
+});
